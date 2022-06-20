44f873102605283c618e58eeaadfc40456d73994
MohamadDamaj <mdamaj12@outlook.com>
2022-06-20 10:26:29 +0100

    a

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,13 @@
 var canvas;
 var ctx;
 
+var gameloop;
+var player;
+
 window.onload = function() {
     canvas = document.getElementById("game-canvas");
     ctx = canvas.getContext("2d");
-
+    player = new player(100, 400);
     ctx.fillStyle = "white" ;
     ctx.fillRect(0,0,1280,720);
 }
