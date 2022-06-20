6f035aafb41d596ce2753757578d00300ef8ef32
MohamadDamaj <mdamaj12@outlook.com>
2022-06-20 11:35:03 +0100

    a

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,18 @@
 var canvas;
 var ctx;
 
+var upKey;
+var rightKey;
+var downKey;
+var leftkey;
+
 var gameloop;
 var player;
 
 window.onload = function() {
     canvas = document.getElementById("game-canvas");
     ctx = canvas.getContext("2d");
+    setupInputs();
     player = new Player(100, 400);
     gameLoop = setInterval(step, 1000/30);
    
@@ -22,4 +28,10 @@ function draw() {
     ctx.fillStyle = "white" ;
     ctx.fillRect(0,0,1280,720);
     player.draw();
+}
+
+function setupInputs() {
+    document.addEventListener("keydown", function(event) {
+
+    });
 }
