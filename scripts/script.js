58f52abb8920163959067c893f635d0667071045
MohamadDamaj <mdamaj12@outlook.com>
2022-06-20 10:37:33 +0100

    a

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,12 @@ var player;
 window.onload = function() {
     canvas = document.getElementById("game-canvas");
     ctx = canvas.getContext("2d");
-    player = new player(100, 400);
+    player = new Player(100, 400);
+    gameLoop = setInterval(step, 1000/30);
     ctx.fillStyle = "white" ;
     ctx.fillRect(0,0,1280,720);
+}
+
+function step() {
+    
 }
