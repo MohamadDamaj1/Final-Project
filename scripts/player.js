5d676cfe5ecc7a01efdd169ed1321e4d13ebd412
MohamadDamaj <mdamaj12@outlook.com>
2022-06-22 15:29:09 +0100

    a

--- scripts/player.js
+++ scripts/player.js
@@ -6,9 +6,14 @@ export class Player {
         this.x = 0;
         this.y = this.game.height - this.height;
         this.image = document.getElementById('player');
+        this.speed =  0;
+        this.maxSpeed = 10;
     }
-    update(){
-        // this.x++;
+    update(input){
+        this.x += this.speed;
+        if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
+        else if (input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
+        else this.speed = 0;
     }
     draw(context){
         context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
