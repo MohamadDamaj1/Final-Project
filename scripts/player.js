fb67f83f804b6a7b90c4c6707b483b8cff0911dd
MohamadDamaj <mdamaj12@outlook.com>
2022-06-22 14:17:20 +0100

    a

--- scripts/player.js
+++ scripts/player.js
@@ -1,5 +1,16 @@
-class Player {
+export class Player {
     constructor(game){
         this.game = game;
+        this.width = 100;
+        this.height = 91.3;
+        this.x = 0;
+        this.y = this.game.height - this.height;
+    }
+    update(){
+
+    }
+    draw(context){
+        context.fillstyle = 'red';
+        context.fillRect(this.x, this.y, this.width, this.height);
     }
 }
