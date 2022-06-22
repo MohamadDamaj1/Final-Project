0be8f822066c46cc953515a7bcd77d9c89e72dec
MohamadDamaj <mdamaj12@outlook.com>
2022-06-22 17:46:52 +0100

    a

--- scripts/player.js
+++ scripts/player.js
@@ -1,4 +1,4 @@
-import { Sitting } from "./playerStates.js";
+import { Sitting, Running, Jumping, Falling } from "./playerStates.js";
 
 export class Player {
     constructor(game){
@@ -12,13 +12,15 @@ export class Player {
         this.image = document.getElementById('player');
         this.frameX = 0;
         this.frameY = 0;
+        this.maxFrame = 5;
         this.speed =  0;
         this.maxSpeed = 10;
-        this.states = [new Sitting(this)];
+        this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)];
         this.currentState = this.states[0];
         this.currentState.enter();
     }
-    update(input){
+    update(input, deltaTime){
+        this.currentState.handleInput(input)
         this.x += this.speed;
         if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
         else if (input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
@@ -26,15 +28,22 @@ export class Player {
         if (this.x < 0) this.x = 0;
         if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
         
-        if (input.includes('ArrowUp') && this.onGround()) this.vy -= 30;
         this.y += this.vy;
         if (!this.onGround()) this.vy += this.weight;
         else this.vy = 0;
+
+        // if (this.frameX < this.maxFrame) this.frameX++;
+        // else this.frameX = 0;
+
     }
     draw(context){
         context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
     }
     onGround(){
         return this.y >= this.game.height - this.height;
+    }
+    setState(state){
+        this.currentState = this.states[state];
+        this.currentState.enter();
     }
 }
