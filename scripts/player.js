b1646cb4788c69c2ab1dc60defc9305ccc684379
MohamadDamaj <mdamaj12@outlook.com>
2022-06-22 14:45:42 +0100

    a

--- scripts/player.js
+++ scripts/player.js
@@ -5,12 +5,12 @@ export class Player {
         this.height = 91.3;
         this.x = 0;
         this.y = this.game.height - this.height;
+        this.image = document.getElementById('player');
     }
     update(){
-
+        // this.x++;
     }
     draw(context){
-        context.fillstyle = 'red';
-        context.fillRect(this.x, this.y, this.width, this.height);
+        context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
     }
 }
