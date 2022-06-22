b1df81ac6eb8e6956a3a4f5377ec68ae65e66a6c
MohamadDamaj <mdamaj12@outlook.com>
2022-06-22 12:07:58 +0100

    a

--- scripts/map.js
+++ scripts/map.js
@@ -1,20 +1,2 @@
-function Border (x, y, width, height, type) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.type = type;
-
-    this.draw = function() {
-        if (this.type === 1) {
-            ctx.fillStyle = "green";
-
-        } else if (this.type === 2) {
-            ctx.fillStyle = "yellow";
-        }
-        ctx.fillRect(this.x, this.y, this.width, this.height)
-    }
-
-}
 
 
