da5a351c42a0433932b8391511ceaadf9ba5d81a
MohamadDamaj <mdamaj12@outlook.com>
2022-06-24 09:52:50 +0100

    a

--- scripts/script.js
+++ scripts/script.js
@@ -18,20 +18,40 @@ window.addEventListener('load', function(){
         this.maxSpeed = 4;
         this.background = new Background(this);
         this.player = new Player(this);
-        this.input = new InputHandler();
+        this.input = new InputHandler(this);
         this.enemies = [];
+        this.enemyTimer = 0;
+        this.enemyInterval = 1000;
+        this.debug = true;
+        this.score = 0;
        
      }
      update(deltaTime){
           this.background.update();
-          this.player.update(this.input.keys, deltaTime);      
+          this.player.update(this.input.keys, deltaTime);
+          if (this.enemyTimer > this.enemyInterval){
+               this.addEnemy();
+               this.enemyTimer = 0;
+          } else {
+               this.enemyTimer += deltaTime;
+          }
+          this.enemies.forEach(enemy => {
+               enemy.update(deltaTime);
+               if (enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy), 1);
+          });     
      }
      draw(context){
         this.background.draw(context);
         this.player.draw(context);
+        this.enemies.forEach(enemy => {
+            enemy.draw(context); 
+        });
      }
      addEnemy(){
-
+         if (this.speed > 0 && Math.random() < 0.5) this.enemies.push(new GroundEnemy(this));
+         else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
+         this.enemies.push(new FlyingEnemy(this));
+         console.log(this.enemies);
      }
   }
 
