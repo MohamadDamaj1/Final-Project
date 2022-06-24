ce6b8d8c94bb73d5a57a656ad66a6f918f77cda5
MohamadDamaj <mdamaj12@outlook.com>
2022-06-24 15:54:45 +0100

    a

--- scripts/script.js
+++ scripts/script.js
@@ -13,19 +13,22 @@ window.addEventListener('load', function(){
      constructor(width, height){
         this.width = width;
         this.height = height;
-        this.groundMargin = 50;
+        this.groundMargin = 80;
         this.speed = 0;
-        this.maxSpeed = 4;
+        this.maxSpeed = 3;
         this.background = new Background(this);
         this.player = new Player(this);
         this.input = new InputHandler(this);
         this.UI = new UI(this);
         this.enemies = [];
+        this.particles = [];
         this.enemyTimer = 0;
         this.enemyInterval = 1000;
         this.debug = true;
         this.score = 0;
         this.fontColor = 'black';
+        this.player.currentState = this.player.states[0];
+        this.player.currentState.enter();
        
      }
      update(deltaTime){
@@ -40,6 +43,10 @@ window.addEventListener('load', function(){
           this.enemies.forEach(enemy => {
                enemy.update(deltaTime);
                if (enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy), 1);
+          });
+          this.particles.forEach((particle, index) => {
+               particle.update();
+               if (particle.markedForDeletion) this.particles.splice(index, 1);
           });     
      }
      draw(context){
@@ -48,18 +55,21 @@ window.addEventListener('load', function(){
         this.enemies.forEach(enemy => {
             enemy.draw(context); 
         });
+        this.particles.forEach(particle => {
+          particle.draw(context);
+        });
         this.UI.draw(context);
      }
      addEnemy(){
          if (this.speed > 0 && Math.random() < 0.5) this.enemies.push(new GroundEnemy(this));
          else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
          this.enemies.push(new FlyingEnemy(this));
-         console.log(this.enemies);
+         
      }
   }
 
   const game = new Game(canvas.width, canvas.height);
-  console.log(game);
+  
   let lastTime = 0;
 
   function animate(timeStamp){
