d5bae47c0120d7ee88ac947a12d5476b16f14a00
MohamadDamaj <mdamaj12@outlook.com>
2022-06-24 14:23:19 +0100

    a

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ import { Player } from './player.js';
 import { InputHandler } from './input.js';
 import { Background } from './background.js';
 import { FlyingEnemy, ClimbingEnemy, GroundEnemy} from './enemies.js';
-
+import { UI } from './UI.js';
 window.addEventListener('load', function(){
   const canvas = document.getElementById('canvas1');
   const ctx = canvas.getContext('2d');
@@ -19,11 +19,13 @@ window.addEventListener('load', function(){
         this.background = new Background(this);
         this.player = new Player(this);
         this.input = new InputHandler(this);
+        this.UI = new UI(this);
         this.enemies = [];
         this.enemyTimer = 0;
         this.enemyInterval = 1000;
         this.debug = true;
         this.score = 0;
+        this.fontColor = 'black';
        
      }
      update(deltaTime){
@@ -46,6 +48,7 @@ window.addEventListener('load', function(){
         this.enemies.forEach(enemy => {
             enemy.draw(context); 
         });
+        this.UI.draw(context);
      }
      addEnemy(){
          if (this.speed > 0 && Math.random() < 0.5) this.enemies.push(new GroundEnemy(this));
